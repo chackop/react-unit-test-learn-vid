8a302666e6eb4a5f9edad2b4d4b077c87c6ccec9
Chacko <chackop@gmail.com>
2019-10-28 00:51:05 +0000

    update dynamic buttons for choosing map

--- src/containers/StoreLocator.js
+++ src/containers/StoreLocator.js
@@ -2,17 +2,45 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 import Button from '../components/Button';
 import Map from '../components/Map';
+import mapChooser from '../mapChooser';
 
 class StoreLocator extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentMap: 'NoneMap.png'
+        };
+        this.shops = [{
+            'location': 'Home',
+            'address': 'Bexley'
+        }, {
+            'location': 'Work',
+            'address': 'London Bridge'
+        }, {
+            'location': '',
+            'address': ''
+        }];
+
+        this.chooseMap = this.chooseMap.bind(this);
+
+    }
+
+    chooseMap(e) {
+        this.setState({ currentMap: mapChooser(e.target.value) });
+    }
+
     render() {
+
+        let storeButtons = this.shops.map((shop, id) => {
+            return (<Button handleClick={this.chooseMap} key={id} location={shop.location} />)
+        });
+
         return (<div>
             <Header />
             <div>
-                <Button location="Home" />
-                <Button location="Work" />
-                <Button />
+                {storeButtons}
             </div>
-            <Map />
+            <Map imagename={this.state.currentMap} location={this.props.location} />
         </div>
         );
     }
--- src/containers/__tests__/StoreLocator.test.js
+++ src/containers/__tests__/StoreLocator.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import {shallow} from 'enzyme';
 import StoreLocator from '../StoreLocator';
 
 describe("StoreLocator", function () {
@@ -18,13 +18,18 @@ describe("StoreLocator", function () {
         expect(headers.length).toBe(1);
     });
 
-    it('renders 3 buttons', () => {
-        const buttons = mountedStoreLocator.find('Button');
-        expect(buttons.length).toBe(3);
-    });
 
     it('renders a map', () => {
         const maps = mountedStoreLocator.find('Map');
         expect(maps.length).toBe(1);
+    })
+});
+
+describe('chooseMap', ()=> {
+    it('updates this.state.currentMap using the location passed to it', ()=>{
+        let mountedStoreLocator = shallow(<StoreLocator />);
+        let mockEvent = {target:{value:'testland'}};
+        mountedStoreLocator.instance().chooseMap(mockEvent);
+        expect(mountedStoreLocator.instance().state.currentMap).toBe('testland.png');
     })
 });
